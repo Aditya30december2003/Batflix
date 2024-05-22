7cab74925c439a7c4554daaf038ba5b8747fc906
Aditya Jain <94634290+Aditya30december2003@users.noreply.github.com>
2024-05-22 11:12:19 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Navbar from './components/Navbar';
-import Main from './components/Main'
-import Row from './components/Row'
-import requests from './Request.js'
 import Footer  from './components/Footer';
+import Signup from './components/SignUp'
+import SignIn from './components/SignIn'
+import Home from './pages/Home'
+import {Routes , Route} from 'react-router-dom'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <Navbar />
-     <Main /> 
-      <Row rowID="1" title='Popular'     fetchURL={requests.requestPopular}/>
-      <Row rowID="2" title='Top-Rated'   fetchURL={requests.requestTopRated}/>
-      <Row rowID="3" title='Upcoming'    fetchURL={requests.requestUpcoming}/>
-      <Row rowID="4" title='Now-Playing' fetchURL={requests.requestNowPlaying}/>
-      <Footer />
+    <Routes >
+    <Route path='/Batflix/signup'  element={<Signup/>}/>
+    <Route path='/Batflix/signin'  element={<SignIn/>}/>
+    <Route path='/Batflix'  element={<Home/>}/>
+    </Routes>
+    <Footer />
     </>
   )
 }
